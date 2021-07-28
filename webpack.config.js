3589b1945c5d7cc7cbc58d9fd8a02b2fc4e03f5c
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-07-28 07:18:49 +0300

    add browsers list

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
-const path = require('path');
+// const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
+let target = 'web';
 
 if (process.env.NODE_ENV === 'production') {
     mode = 'production';
+    target = 'browserslist';
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     module: {
         rules: [
