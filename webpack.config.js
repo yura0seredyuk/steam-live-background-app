54a51391072182657dcf372d39bd67d7c4626c59
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-07-29 19:23:30 +0300

    add open in same tab (Chrome)

--- webpack.config.js
+++ webpack.config.js
@@ -5,130 +5,139 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const path = require('path');
 const StylelintPlugin = require('stylelint-webpack-plugin');
+const openBrowser = require('react-dev-utils/openBrowser');
 
 let mode = 'development';
 let target = 'web';
 
+const protocol = 'http:';
+const host = process.env.HOST || 'localhost';
+const port = parseInt(process.env.PORT, 10) || 3000;
+
 if (process.env.NODE_ENV === 'production') {
-    mode = 'production';
-    target = 'browserslist';
+  mode = 'production';
+  target = 'browserslist';
 }
 
 // console.log('Mode:', mode);
 
 const Loader = () => {
-    const loader = ['babel-loader'];
+  const loader = ['babel-loader'];
 
-    if (mode === 'development') {
-        loader.push('eslint-loader')
-    }
+  if (mode === 'development') {
+    loader.push('eslint-loader')
+  }
 
-    return loader;
+  return loader;
 }
 
+
 const Plugins = () => {
-    const plugins = [
-          new MiniCssExtractPlugin(),
-          new HtmlWebpackPlugin({
-              template: './src/index.html',
-          }),
-          new CleanWebpackPlugin(),
-          new CopyWebpackPlugin({
-              patterns: [
-                  { from: path.resolve(__dirname, 'src', 'favicon.ico'), to: path.resolve(__dirname, 'dist') },
-              ],
-          }),
-      ];
+  const plugins = [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, 'src', 'favicon.ico'), to: path.resolve(__dirname, 'dist') },
+      ],
+    }),
+  ];
 
-    if (mode === 'development') {
-        plugins.push(new StylelintPlugin());
-    }
+  if (mode === 'development') {
+    plugins.push(new StylelintPlugin());
+  }
 
-    if (mode === 'production') {
-        plugins.push(new BundleAnalyzerPlugin());
-    }
+  if (mode === 'production') {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
 
-    return plugins;
+  return plugins;
 }
 
 module.exports = {
-    mode: mode,
-    target: target,
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        // publicPath: "/dist/",
-        // assetModuleFilename: 'assets/[name][ext][query]'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource', // or asset
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)/,
-                type: 'asset/resource',
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'fonts/',
-                            publicPath: path.resolve(__dirname, 'dist', 'fonts/')
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '',
-                        }
-                    },
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: Loader()
-            },
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: Loader()
+  mode: mode,
+  target: target,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    // publicPath: "/dist/",
+    // assetModuleFilename: 'assets/[name][ext][query]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource', // or asset
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)/,
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+              publicPath: path.resolve(__dirname, 'dist', 'fonts/')
             }
+          }
         ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'], // add other ./...
-        alias: {
-            '@': path.resolve(__dirname, 'src') // add new alias
-        }
-    },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
-    plugins: Plugins(),
-    devtool: 'source-map',
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4000',
-            },
-            pathRewrite: { '^/api': '' },
-        },
-        // open: 'Google Chrome',
-        port: 3000,
-        contentBase: './dist',
-        hot: true,
-        historyApiFallback: true,
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            }
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: Loader()
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: Loader()
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // add other ./...
+    alias: {
+      '@': path.resolve(__dirname, 'src') // add new alias
     }
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: Plugins(),
+  devtool: 'source-map',
+  devServer: {
+    onListening: () => {
+      openBrowser(`${protocol}//${host}:${port}`);
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+      },
+      pathRewrite: { '^/api': '' },
+    },
+    // open: 'Google Chrome',
+    port: 3000,
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true,
+  }
 }
